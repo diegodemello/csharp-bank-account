ed4a6c11dd403fb4274d325f5e487cc4d462751a
Diego <diegodemello027@gmail.com>
2024-12-08 10:42:34 -0300

    Adicionar arquivos de projeto.

---
+++ BankAccountManager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankAccountManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
